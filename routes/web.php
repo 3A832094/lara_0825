466bf8b569d4345412255a08afd86daf488bb3da
3A832094 <3a832094@gm.student.ncut.edu.tw>
2021-11-14 14:41:24 +0800

    lara_0825_u1 練習6：Model V.S. Collection

--- routes/web.php
+++ routes/web.php
@@ -54,4 +54,16 @@ Route::get('contact', [\App\Http\Controllers\PostsController::class, 'contact'])
 //$post = Post::find(1);
 //$post->delete();
 //Post::destroy(2);
-Post::destroy(3,5,7);
+//Post::destroy(3,5,7);
+//---------------------------------------
+//$allPosts = Post::all();
+//dd($allPosts);
+//---------------------------------------
+//$featurePosts = Post::where('is_feature', 1)->get();
+//dd($featurePosts);
+//---------------------------------------
+//$fourthPost = Post::find(4);
+//dd($fourthPost);
+//---------------------------------------
+$lastPost = Post::orderBy('id', 'DESC')->first();
+dd($lastPost);
